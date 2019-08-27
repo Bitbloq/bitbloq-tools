d14e8ad879685e676d9b2c663314c86050ddc04c
kikeelectronico <enrique.gomez.electronico@gmail.com>
2019-08-27 12:52:11 +0200

    Kick of

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bitbloq-tools
 A set of python bitbloq tools to work with documents
+
+# JSOn viewer
+You must install a JSON viewer in Google Chrome.
+
+[JSONView](https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc) could be an option. If JSON viewer is used, you must enaable `Permitir acceso a URL de archivo` or `Allow access to file URL` from [JSON config page](chrome://extensions/?id=chklaanhfefbnpoihckbnefhakgolnmc)
+
+# How to
+1. Open a terminal and go to `src` directory
+
+```
+cd src
+```
+
+2. Copy your `.3d.bitbloq` files into `src` directory.
+
+3. Run the tool:
+
+```
+ python bitbloq_tools.py
+```
---
+++ src/bitbloq_tools.py
@@ -0,0 +1,104 @@
+import json
+from os import listdir, system
+from os.path import isfile, join
+import random
+import string
+import webbrowser
+
+message = ["","Mergeando la vida", "La vida es vella"]
+
+def remove_char(s):
+    return s[ 1:len(s) - 1]
+
+def change(id):
+    while(True):
+        newId = id[0:len(id) - 1] + str(random.randint(0,10000))
+        if not newId in originIDs:
+            originIDs.append(newId)
+            return newId
+#Show menu options
+system('clear')
+print("Tools:")
+print("1 - Merger")
+print("2 - Prettify")
+#Ask the users
+print("\r\n")
+tool = int(input("Choose a tool: "))
+
+#Read files
+onlyfiles = [f for f in listdir("./") if isfile(join("./", f))]
+#Show files
+system('clear')
+print(message[tool])
+index = 0
+for file in onlyfiles:
+    print(str(index) + " - " + file);
+    index += 1
+
+if tool == 1:
+    #Ask the user
+    print("\r\n")
+    baseFile = int(input("Choose a base file: "))
+    contentFile = int(input("Choose a secondary file: "))
+
+    originIDs = []
+    new = {}
+    contents = []
+
+    #Open base file
+    with open(onlyfiles[baseFile]) as json_file:
+        data = json.load(json_file)
+        new = data
+        contents = json.loads(data["content"])
+        #Get all parents ids
+        for o in contents:
+            originIDs.append(o["id"])
+
+    #Open secondary file
+    with open(onlyfiles[contentFile]) as json_file:
+        data = json.load(json_file)
+        secondaryContents = json.loads(data["content"])
+        #Verify first level IDs
+        for o in secondaryContents:
+            #Change the id if it is repeated
+            if o["id"] in originIDs:
+                local = o
+                local["id"] = change(local["id"])
+                contents.append(o)
+            else:
+                contents.append(o)
+
+    #Append content to new file and change the document title
+    new["content"] = json.dumps(contents)
+    new["title"] = "Merge"
+    #Save the new file
+    with open('merge.3d.bitbloq', 'w') as outfile:
+        json.dump(new, outfile)
+
+elif tool == 2:
+    #Ask the user
+    print("\r\n")
+    baseFile = int(input("Elige el archivo: "))
+
+    #Read the file
+    data = {}
+    with open(onlyfiles[baseFile]) as json_file:
+        data = json.load(json_file)
+        #Format the content string
+        content = data["content"]
+        data["content"] = json.loads(content)
+
+    #print(json.dumps(data, indent=4, sort_keys=True));
+
+    #Change document title and file name
+    data["title"] = data["title"] + "_pretty"
+    name = onlyfiles[baseFile].split(".")[0] + "_pretty.json"
+    #Save the document as JSON
+    with open(name, 'w') as outfile:
+        json.dump(data, outfile)
+
+    #Open the webbrowser
+    chrome_path = '/usr/bin/google-chrome %s'
+    file_path = "./" + name
+    webbrowser.get(chrome_path).open(file_path)
+    print("\r\nOpening in Google Chrome. Remember to install a JSON viewer in Chrome.");
